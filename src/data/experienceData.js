e191d250ca9e17403b032f66c3eeccd64c23ff3d
SATKIRTI MISHRA <87634054+SATKIRTI@users.noreply.github.com>
2023-05-27 17:41:37 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,6 +1,13 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'GirlScript Summer Of Code',
+        jobtitle: 'Contributor at GSSOC 2023',
+        startYear: 'May 2023',
+        endYear: 'Present',
+    },
+    {
+        id: 1,
         company: 'MICROSOFT',
         jobtitle: 'Microsoft Engage 2022',
         startYear: 'May 2022',
