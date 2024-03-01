3128f0f3309c4b7bad6b96c78a03dbe1cf6bcdd1
SATKIRTI MISHRA <87634054+SATKIRTI@users.noreply.github.com>
2024-03-01 11:56:06 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -7,11 +7,19 @@ import six from '../assets/svg/projects/six.svg'
 import seven from '../assets/svg/projects/seven.svg'
 import eight from '../assets/svg/projects/eight.svg'
 import nine from '../assets/svg/projects/eight.svg'
+import ten from '../assets/svg/projects/nine.svg'
 
 
 export const projectsData = [
     {
         id: 1,
+        projectName: 'DairyDelight',
+        projectDesc: 'This project is a MERN-stack web-based application.DairyDelight aims to revolutionize the way dairy products are managed, bought, and sold. 
+        demo: 'https://dairydelightapp.cyclic.app/',
+        image: ten
+    },
+    {
+        id: 2,
         projectName: 'WeatherNow',
         projectDesc: 'This project is a web-based weather app that allows users to get real-time weather information for any location. The app has a simple and intuitive interface that allows users to quickly enter a location and view the current weather conditions.Used Openweather api to build this project.',
         tags: ['ReactJS', 'Axios', 'Moment.js', 'CSS', 'API'],
@@ -20,7 +28,7 @@ export const projectsData = [
         image: one
     },
     {
-        id: 2,
+        id: 3,
         projectName: 'ShowCase',
         projectDesc: 'The website is a platform for showcasing my talents, skills, and accomplishments in a professional and visually appealing manner.',
         tags: ['ReactJS', 'HTML', 'CSS', 'API(SheetDB)', 'Material-UI'],
@@ -29,7 +37,7 @@ export const projectsData = [
         image: two
     },
     {
-        id: 3,
+        id: 4,
         projectName: 'QuizMaster',
         projectDesc: 'Developed a quiz app using ReactJS, CSS, and Material UI, with an intuitive interface and responsive design.Implemented React fundamentals, including component composition, state management, and event handling, to create a dynamic and engaging user experience.',
         tags: ['ReactJS', 'CSS', 'Material Ui'],
@@ -38,7 +46,7 @@ export const projectsData = [
         image: three
     },
     {
-        id: 4,
+        id: 5,
         projectName: 'Facial Attend',
         projectDesc: 'The system is designed to accurately identify and track individuals using their facial features, improving efficiency and reducing errors in the attendance-taking process.Ability to store and manage attendance records in a database.It is a user-friendly interface for instructors to view attendance records',
         tags: ['Python', 'OpenCV', 'SQLite', 'GUI'],
@@ -47,7 +55,7 @@ export const projectsData = [
         image: four
     },
     {
-        id: 5,
+        id: 6,
         projectName: 'KeepIt',
         projectDesc: '"KeepIt" is a possible name for a Google Keep clone project that utilizes ReactJS and Firebase. It is likely a web-based application that allows users to create, edit, and manage notes, similar to the functionality of Google Keep. The use of ReactJS suggests that the project utilizes a component-based architecture for building the user interface. Firebase is a popular backend-as-a-service platform that provides real-time data synchronization and authentication features.',
         tags: ['ReactJS', 'Firebase'],
@@ -56,7 +64,7 @@ export const projectsData = [
         image: five
     },
     {
-        id: 6,
+        id: 7,
         projectName: '  TodoLister',
         projectDesc: 'Built a Todo app clone using ReactJS to manage tasks efficiently. Designed a simple and intuitive user interface using HTML, CSS, and Bootstrap framework.Implemented React components for various functionalities like adding, editing, and deleting tasks. Utilized React state management to ensure smooth updates of tasks and their completion status.',
         tags: ['ReactJS'],
@@ -65,7 +73,7 @@ export const projectsData = [
         image: six
     },
     {
-        id: 7,
+        id: 8,
         projectName: 'ClickCounter',
         projectDesc: 'This is a simple counter app built using ReactJS that allows users to increase or decrease a number by clicking on corresponding buttons. The app has an intuitive and user-friendly interface that displays the current count value in real-time, and updates the value automatically with each click.This app is useful for anyone who needs a quick and easy way to keep track of counts or scores in a variety of contexts.',
         tags: ['ReactJS'],
@@ -74,7 +82,7 @@ export const projectsData = [
         image: seven
     },
     {
-        id: 8,
+        id: 9,
         projectName: 'Dragon Clash',
         projectDesc: 'Uber clone',
         tags: ['JavaScript', 'HTML', 'CSS'],
@@ -83,7 +91,7 @@ export const projectsData = [
         image: eight
     },
     {
-        id: 9,
+        id: 10,
         projectName: 'Food-App',
         projectDesc: 'It is a responsive layout that adjusts to different screen sizes and devices. There is a navigation menu that allows users to browse different sections of the website, such as the menu, locations, and specials.It has search bar that lets users search for specific items or keywords within the website.There is a menu section that displays different food categories, such as chicken, sandwiches, sides, and desserts.',
         tags: ['JavaScript', 'HTML', 'CSS'],
