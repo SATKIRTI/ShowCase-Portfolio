686430b1240b5185b41929678c4abd95acd81f62
SATKIRTI MISHRA <87634054+SATKIRTI@users.noreply.github.com>
2024-03-01 12:12:06 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,4 @@
-import resume from 'https://drive.google.com/file/d/1CnNF_8fdxJnbtrbdCTSjQwbbG69AnBuP/view'
+import resume from 'file:///C:/Users/Dell/Downloads/Satkirti_Mishra_ResumeNew.pdf'
 import profileimg from '../assets/profilepic (2).png'
 
 
@@ -6,7 +6,7 @@ import profileimg from '../assets/profilepic (2).png'
 export const headerData = {
     name: 'Satkirti Mishra',
     title: "Web Developer",
-    desciption:"I am a pre-final year student with a Bachelor's degree in Information Technology.I worked at Microsoft where I worked on a live project related to facial recognition. In my free time, I enjoy solving coding problems on platforms like Leetcode and GeeksforGeeks. I am excited to apply my technical skills and knowledge to contribute to a team and learn new things in a professional setting.",
+    desciption:"I am a final year student with a Bachelor's degree in Information Technology.I worked at Microsoft where I worked on a live project related to facial recognition. In my free time, I enjoy solving coding problems on platforms like Leetcode and GeeksforGeeks. I am excited to apply my technical skills and knowledge to contribute to a team and learn new things in a professional setting.",
     resumePdf: resume,
     image: profileimg
 }
